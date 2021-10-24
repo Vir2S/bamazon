1b213fb96e6b0ca64d22223e1d008ceb9119c58f
vir2s <virtualik@gmail.com>
2021-10-24 16:20:58 +0300

    books models and app added

--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+        ordering = ("id", )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    number_of_pages = models.PositiveSmallIntegerField(default=0)
+    authors = models.ManyToManyField(Author, blank=True)
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+        ordering = ("id", )
+
+    def __str__(self):
+        return self.title
--- bamazon/settings.py
+++ bamazon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'apps.accounts',
+    'apps.books',
 
     "debug_toolbar",
     "rest_framework",
